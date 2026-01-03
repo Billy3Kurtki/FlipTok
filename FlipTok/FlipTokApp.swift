5ceb3f5305996464c8dabe76cc6dd72ded0adf27
Кирилл Казаков <kirill.kazakov26@gmail.com>
2026-01-03 19:02:08 +0300

    Initial Commit

---
+++ FlipTok/FlipTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipTokApp.swift
+//  FlipTok
+//
+//  Created by Кирилл Казаков on 03.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct FlipTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
